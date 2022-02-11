4fa2dcd5c3bcf3a29c555b2ff51d78187ce03d91
Diego <d.2120leandro@gmail.com>
2022-02-11 17:35:36 -0300

    Contador

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+var numberhtml = document.querySelector('.number')
+var startnumber = 0;
+
+function plus () {
+    startnumber = startnumber + 1;
+    numberhtml.innerHTML = startnumber;
+   
+}
+
+
+
+function minus () {
+    startnumber = startnumber - 1;
+    numberhtml.innerHTML = startnumber;
+  
+}
+
